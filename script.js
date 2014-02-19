b3e0cb43a10038cff4692e78c170f4fcf682d12e
Valentyn Khatsaiuk <mok.crimea@gmail.com>
2014-02-19 00:01:24 +0000

    изменил алгоритм работы. теперь все работает без разбиения на отдельные кластеры.

--- script.js
+++ script.js
@@ -64,12 +64,12 @@ function control() {
 
     function fillRandomMatrix() {
         var a = [];
-        for (var i = 0; i < L; i++) {
+        for (var i = 0; i < L + 1; i++) {
             a[i] = new Array(L);
-            for (var j = 0; j < L; j++) {
-                if (i === 0) {
+            for (var j = 0; j < L + 1; j++) {
+                if (i === 0 || i === L) {
                     a[i][j] = 0;
-                } else if (j === 0) {
+                } else if (j === 0 || j === L) {
                     a[i][j] = 0;
                 } else {
                     a[i][j] = Math.random() < prob ? 1 : 0;
@@ -79,7 +79,7 @@ function control() {
         return a;
     }
 
-    function checkClusters() {
+    /*    function checkClusters() {
         var goodCL = [];
         var count = 0;
         for (var i = 1; i < L; i++) {
@@ -129,7 +129,111 @@ function control() {
             }
         }
     }
+*/
 
+    function checkClusters() {
+        for (var j = 1; j < L; j++) {
+            i = 1;
+            if (a[i][j] == 1) {
+                a[i][j] = 2;
+            }
+        }
+
+        var temp = 0;
+        while (temp <= 5) {
+            checkDown();
+            checkUP();
+            temp++;
+        }
+
+
+        function checkDown() {
+            for (var i = 1; i < L; i++) {
+                for (var j = 1; j < L; j++) {
+                    var cur = a[i][j],
+                        up = a[i - 1][j],
+                        left = a[i][j - 1];
+                    var right = a[i][j + 1];
+                    var down = a[i + 1][j];
+
+                    if (cur !== 0) {
+                        if (cur === 1) {
+                            if (up === 2 || left === 2 || right === 2 || down === 2) {
+                                a[i][j] = 2;
+                            }
+                        }
+                    }
+                }
+
+                for (var j = L - 1; j > 0; j--) {
+                    var cur = a[i][j],
+                        up = a[i - 1][j],
+                        left = a[i][j - 1];
+                    var right = a[i][j + 1];
+                    var down = a[i + 1][j];
+
+                    if (cur !== 0) {
+                        if (cur === 1) {
+                            if (up === 2 || left === 2 || right === 2 || down === 2) {
+                                a[i][j] = 2;
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+        function checkUP() {
+            for (var i = L - 1; i > 0; i--) {
+                for (var j = 1; j < L; j++) {
+                    var cur = a[i][j],
+                        up = a[i - 1][j],
+                        left = a[i][j - 1];
+                    var right = a[i][j + 1];
+                    var down = a[i + 1][j];
+
+                    if (cur !== 0) {
+                        if (cur === 1) {
+                            if (up === 2 || left === 2 || right === 2 || down === 2) {
+                                a[i][j] = 2;
+                            }
+                        }
+                    }
+                }
+
+                for (var j = L - 1; j > 0; j--) {
+                    var cur = a[i][j],
+                        up = a[i - 1][j],
+                        left = a[i][j - 1];
+                    var right = a[i][j + 1];
+                    var down = a[i + 1][j];
+
+                    if (cur !== 0) {
+                        if (cur === 1) {
+                            if (up === 2 || left === 2 || right === 2 || down === 2) {
+                                a[i][j] = 2;
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+
+
+        // var str = '';
+        // for (var j = 0; j < L; j++) {
+        //     var i = L - 1;
+        //     str += a[i][j] + '\t';
+        // }
+        // console.log(str);
+        // console.log("\n");
+
+
+
+    }
 
     function leftClusters() {
         //Выделяем только существующие кластеры
@@ -148,25 +252,17 @@ function control() {
     }
 
     function isPercolation() {
-        var leftColumn = [];
-        var rightColumn = [];
+        var topColumn = [];
+        var downColumn = [];
+        var goal;
         // Составим список кластеров находящийся у левой грани и у правой грани.
-        for (var i = 0; i < L; i++) {
-            for (var j = 0; j < L; j++) {
-                if (j == 1 && a[i][j] !== 0) {
-                    leftColumn.push(a[i][j]);
-                } else if (j == L - 1 && a[i][j] !== 0) {
-                    rightColumn.push(a[i][j]);
-                }
+        for (var j = 0; j < L; j++) {
+            var i = L - 1;
+            if (a[i][j] === 2) {
+                goal = true;
             }
         }
 
-        var goal;
-        leftColumn.forEach(function(clusterNum) {
-            if (rightColumn.indexOf(clusterNum) !== -1) {
-                goal = clusterNum;
-            }
-        });
         return goal;
 
     }
@@ -184,13 +280,23 @@ function control() {
             }
         });
 
-        if (goal !== undefined) {
+        if (goal) {
             for (var i = 0; i < L; i++) {
                 for (var j = 0; j < L; j++) {
-                    if (a[i][j] === goal) {
+                    if (a[i][j] === 2) {
                         context.fillRect((j - 1) * pixel, (i - 1) * pixel, pixel, pixel);
                     }
-                    context.fillStyle = '#000';
+                    context.fillStyle = 'aqua';
+
+                }
+            }
+        } else {
+            for (var i = 0; i < L; i++) {
+                for (var j = 0; j < L; j++) {
+                    if (a[i][j] === 2) {
+                        context.fillRect((j - 1) * pixel, (i - 1) * pixel, pixel, pixel);
+                    }
+                    context.fillStyle = 'orange';
 
                 }
             }
@@ -209,10 +315,8 @@ function control() {
         }
     }
 
-    if (goal !== undefined) {
+    if (goal) {
         return true;
-    } else {
-        return false;
     }
-
+    return false;
 }
